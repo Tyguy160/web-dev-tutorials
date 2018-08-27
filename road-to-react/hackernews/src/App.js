f9ab1e8a40ef84087ab355b2390a5e80554ef2b1
Tyler Stephen Maschino <tylermaschino@gmail.com>
2018-08-27 05:46:07 -0400

    Created Search and Table components

--- road-to-react/hackernews/src/App.js
+++ road-to-react/hackernews/src/App.js
@@ -9,7 +9,7 @@ const list = [
     author: "Jordan Walke",
     num_comments: 3,
     points: 4,
-    objectID: 0
+    objectID: 0,
   },
   {
     title: "Redux",
@@ -17,16 +17,62 @@ const list = [
     author: "Dan Abramov, Andrew Clark",
     num_comments: 2,
     points: 5,
-    objectID: 1
-  }
+    objectID: 1,
+  },
 ];
 
+const isSearched = searchTerm => item =>
+  item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list,
+      searchTerm: "",
+    };
+    this.onDismiss = this.onDismiss.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
+  }
+  onDismiss(id) {
+    const isNotId = item => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({ list: updatedList });
+  }
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
   render() {
-    const helloWorld = "Welcome to the Road to learn React";
+    const { searchTerm, list } = this.state;
     return (
       <div className="App">
-        {list.map(item => (
+        <Search value={searchTerm} onChange={this.onSearchChange}>
+          Search
+        </Search>
+        <Table list={list} pattern={searchTerm} onDismiss={this.onDismiss} />
+      </div>
+    );
+  }
+}
+
+class Search extends Component {
+  render() {
+    const { value, onChange, children } = this.props;
+    return (
+      <form>
+        {children}
+        <input type="text" value={value} onChange={onChange} />
+      </form>
+    );
+  }
+}
+
+class Table extends Component {
+  render() {
+    const { list, pattern, onDismiss } = this.props;
+    return (
+      <div>
+        {list.filter(isSearched(pattern)).map(item => (
           <div key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}</a>
@@ -34,6 +80,11 @@ class App extends Component {
             <span>{item.author}</span>
             <span>{item.num_comments}</span>
             <span>{item.points}</span>
+            <span>
+              <button onClick={() => onDismiss(item.objectID)} type="button">
+                Dismiss
+              </button>
+            </span>
           </div>
         ))}
       </div>
