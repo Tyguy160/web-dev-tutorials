5d828cb6546b2ca3ac70cb4151ec9d78bf782cc1
Tyler Maschino <tylermaschino@gmail.com>
2018-12-02 08:41:30 -0500

    Added styling to the Header component

--- Advanced-React-master/sick-fits/frontend/components/Header.js
+++ Advanced-React-master/sick-fits/frontend/components/Header.js
@@ -23,9 +23,28 @@ const Logo = styled.h1`
   }
 `;
 
+const StyledHeader = styled.header`
+  .bar {
+    border-bottom: 10px solid ${props => props.theme.black};
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+    align-items: stretch;
+    @media (max-width: 1300px) {
+      grid-template-columns: auto 1fr;
+      justify-content: center;
+    }
+  }
+  .sub-bar {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    border-bottom: 1px solid ${props => props.theme.lightGrey};
+  }
+`;
+
 const Header = () => {
   return (
-    <div>
+    <StyledHeader>
       <div className="bar">
         <Logo>
           <Link href="/">
@@ -38,7 +57,7 @@ const Header = () => {
         <p>Search</p>
       </div>
       <div>Cart</div>
-    </div>
+    </StyledHeader>
   );
 };
 
