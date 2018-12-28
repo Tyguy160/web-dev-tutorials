5f425084505f2cc779c92ede087aa04023d32482
Tyler Maschino <Tylermaschino@gmail.com>
2018-12-28 07:38:10 -0500

    Restarting the tutorial

--- Advanced-React-master/sick-fits/backend/src/resolvers/Mutation.js
+++ Advanced-React-master/sick-fits/backend/src/resolvers/Mutation.js
@@ -1,28 +1,3 @@
-const Mutations = {
-  async createItem(parent, args, ctx, info) {
-    // TODO: Check if they are logged in
+const mutations = {};
 
-    const item = await ctx.db.mutation.createItem(
-      {
-        data: {
-          ...args
-        }
-      },
-      info
-    );
-
-    console.log(item);
-
-    return item;
-  },
-  updateItem(parent, args, ctx, info) {
-    // first take a copy of the updates
-    const updates = { ...args };
-    // remove the ID from the updates
-    delete updates.id;
-    // run the update method
-    return ctx.db.mutation.updateItem;
-  }
-};
-
-module.exports = Mutations;
+module.exports = mutations;
--- Advanced-React-master/sick-fits/backend/src/resolvers/Query.js
+++ Advanced-React-master/sick-fits/backend/src/resolvers/Query.js
@@ -1,13 +1,3 @@
-const { forwardTo } = require('prisma-binding');
-
-const Query = {
-  items: forwardTo('db'),
-  item: forwardTo('db')
-  // async items(parent, args, ctx, info) {
-  //   const items = await ctx.db.query.items();
-  //   console.log(items);
-  //   return items;
-  // }
-};
+const Query = {};
 
 module.exports = Query;
