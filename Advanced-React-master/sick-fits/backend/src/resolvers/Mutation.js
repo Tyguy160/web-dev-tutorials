03d56e41cf5754225f5c70570b68a3ab2b99a88a
Tyler Maschino <tylermaschino@gmail.com>
2019-01-14 20:05:12 -0500

    Added new mutations and queries for updating and deleting

--- Advanced-React-master/sick-fits/backend/src/resolvers/Mutation.js
+++ Advanced-React-master/sick-fits/backend/src/resolvers/Mutation.js
@@ -13,6 +13,30 @@ const Mutations = {
 
     return item;
   },
+
+  async updateItem(parent, args, context, info) {
+    const updates = { ...args };
+    delete updates.id;
+    console.log(args);
+    const item = await context.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id,
+        },
+      },
+      info
+    );
+    return item;
+  },
+
+  async deleteItem(parent, args, context, info) {
+    const where = { id: args.id };
+
+    const item = await context.db.query.item({ where }, `{id, title}`);
+
+    return context.db.mutation.deleteItem({ where }, info);
+  },
 };
 
 module.exports = Mutations;
--- Advanced-React-master/sick-fits/backend/src/resolvers/Query.js
+++ Advanced-React-master/sick-fits/backend/src/resolvers/Query.js
@@ -4,6 +4,7 @@ const Query = {
   // This forwards API call straight to the DB
   // instead of creating custom resolver shown below
   items: forwardTo('db'),
+  item: forwardTo('db'),
   //   async items(parent, args, context, info) {
   //     const items = await context.db.query.items();
   //     return items;
